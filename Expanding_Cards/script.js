2d56cc2dadb94affc06cdc371369a56928895f18
Asadijaz786 <ijazasad20@gmail.com>
2022-04-20 02:31:48 +0500

    Expanding Cards: javascript
     project1

---
+++ Expanding_Cards/script.js
@@ -0,0 +1,15 @@
+const panels = document.querySelectorAll(".panel");
+const panelActive = document.querySelector(".active panel");
+
+panels.forEach((panel) => {
+  panel.addEventListener("click", () => {
+    removeActiveClasses();
+    panel.classList.add("active");
+  });
+});
+
+function removeActiveClasses() {
+  panels.forEach((panel) => {
+    panel.classList.remove("active");
+  });
+}
