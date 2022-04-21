9578cd9a620ac4528a4363d527d919eb73b8e75f
Asadijaz786 <ijazasad20@gmail.com>
2022-04-22 01:38:56 +0500

    Hidden Search: javascript
     project4

--- Hidden_Search/script.js
+++ Hidden_Search/script.js
@@ -1,1 +1,8 @@
-const progress = document.getElementById("progress");
+const search = document.querySelector(".search");
+const btn = document.querySelector(".btn");
+const input = document.querySelector(".input");
+
+btn.addEventListener("click", () => {
+  search.classList.toggle("active");
+  input.focus();
+});
