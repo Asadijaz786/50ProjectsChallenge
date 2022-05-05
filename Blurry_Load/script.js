18f50b40d8664a63b92a1b61f28b286837fa779b
Asadijaz786 <ijazasad20@gmail.com>
2022-05-05 22:28:47 +0500

    Blurry Load: JavaScript
     project5

--- Blurry_Load/script.js
+++ Blurry_Load/script.js
@@ -1,9 +1,22 @@
 const loadText = document.querySelector(".loading-text");
-const bg = document.querySelector(".bg");
+const bgd = document.querySelector(".bgd");
 
 let load = 0;
 
 let int = setInterval(blurry, 30);
+
 function blurry() {
   load++;
+
+  if (load > 99) {
+    clearInterval(int);
+  }
+
+  loadText.innerText = `${load}%`;
+  loadText.style.opacity = scale(load, 0, 100, 1, 0);
+  bgd.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`;
 }
+
+const scale = (number, inMin, inMax, outMin, outMax) => {
+  return ((number - inMin) * (outMax - outMin)) / (inMax - inMin) + outMin;
+};
