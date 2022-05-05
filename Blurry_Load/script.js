b258dd393e3c0a96378b9dfc0255e33964ad3efa
Asadijaz786 <ijazasad20@gmail.com>
2022-05-05 22:00:35 +0500

    Blurry Load: HTML&CSS slight changes
     project5

--- Blurry_Load/script.js
+++ Blurry_Load/script.js
@@ -1,1 +1,9 @@
-const progress = document.getElementById("progress");
+const loadText = document.querySelector(".loading-text");
+const bg = document.querySelector(".bg");
+
+let load = 0;
+
+let int = setInterval(blurry, 30);
+function blurry() {
+  load++;
+}
