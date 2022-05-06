fa24cde00eba4bdebb0a4e901b7eb53bca17bc94
Asadijaz786 <ijazasad20@gmail.com>
2022-05-06 23:14:25 +0500

    GitHub Profile: JavaScript_1
     project6

--- Github_Profile/script.js
+++ Github_Profile/script.js
@@ -1,1 +1,62 @@
-const progress = document.getElementById("progress");
+const APIURL = "https://api.github.com/users/";
+
+const main = document.getElementById("main");
+const search = document.getElementById("search");
+const form = document.getElementById("form");
+
+async function getUser(username) {
+  try {
+    const { data } = await axios(APIURL + username);
+    createUserCard(data);
+  } catch (err) {
+    if (err.response.status == 404) {
+      createErrorCard("No User Found with username: " + username);
+    }
+  }
+}
+
+function createUserCard(user) {
+  const cardHTML = ` <div class="card">
+  <div>
+    <img
+      src="${user.avatar_url}"
+      alt="${user.name}"
+      class="avatar"
+    />
+  </div>
+  <div class="user-info">
+    <h2>${user.name}</h2>
+    <p>
+    ${user.bio}
+    </p>
+    <ul>
+      <li>${user.followers}<strong> Followers</strong></li>
+      <li>${user.following}<strong> Following</strong></li>
+      <li>${user.public_repos}<strong> Repos</strong></li>
+    </ul>
+    <div id="repos"></div>
+  </div>
+</div>`;
+
+  main.innerHTML = cardHTML;
+}
+
+function createErrorCard(errorMsg) {
+  const cardHTML = `
+  <div class='card'>
+  <h1>${errorMsg}</h1>
+  </div>`;
+
+  main.innerHTML = cardHTML;
+}
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const user = search.value;
+
+  if (user) {
+    getUser(user);
+    search.value = "";
+  }
+});
