65df10f78a875eca1d09776c8ce9fbefcea44120
Asadijaz786 <ijazasad20@gmail.com>
2022-05-07 22:32:58 +0500

    Scroll Animation: javascript
     project7

--- Scroll_Animation/script.js
+++ Scroll_Animation/script.js
@@ -1,1 +1,19 @@
-const progress = document.getElementById("progress");
+const boxes = document.querySelectorAll(".box");
+
+window.addEventListener("scroll", checkBoxes);
+
+checkBoxes();
+
+function checkBoxes() {
+  const triggerBottom = (window.innerHeight / 5) * 4;
+
+  boxes.forEach((box) => {
+    const boxTop = box.getBoundingClientRect().top;
+
+    if (boxTop < triggerBottom) {
+      box.classList.add("show");
+    } else {
+      box.classList.remove("show");
+    }
+  });
+}
