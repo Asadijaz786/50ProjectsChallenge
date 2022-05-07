0678a77ed3f5dbc4f0e8cfd64638d864e4e6431c
Asadijaz786 <ijazasad20@gmail.com>
2022-05-08 00:02:10 +0500

    Form Wave Animation: simple form
     project8

--- Form_Wave_Animation/script.js
+++ Form_Wave_Animation/script.js
@@ -1,1 +1,8 @@
-const progress = document.getElementById("progress");
+const labels = document.querySelectorAll(".form-control label");
+
+labels.forEach((label) => {
+  label.innerHTML = label.innerText
+    .split("")
+    .map((letter, idx) => `<span>${letter}</span>`)
+    .join("");
+});
