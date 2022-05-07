176c18d907334d1df5867900ab28015a3c8f6f80
Asadijaz786 <ijazasad20@gmail.com>
2022-05-08 01:10:08 +0500

    Jokes_Daddy: JavaScript
     project9

--- Jokes_Daddy/script.js
+++ Jokes_Daddy/script.js
@@ -1,1 +1,34 @@
-const progress = document.getElementById("progress");
+const jokeEl = document.getElementById("joke");
+const jokeBtn = document.getElementById("jokeBtn");
+
+jokeBtn.addEventListener("click", generateJoke);
+
+generateJoke();
+
+// Using Async/Await
+async function generateJoke() {
+  const config = {
+    headers: {
+      Accept: "application/json",
+    },
+  };
+  const res = await fetch("https://icanhazdadjoke.com", config);
+
+  const data = await res.json();
+  jokeEl.innerHTML = data.joke;
+}
+
+//  Using .then
+// function generateJoke() {
+//   const config = {
+//     headers: {
+//       Accept: "application/json",
+//     },
+//   };
+//   fetch("https://icanhazdadjoke.com", config)
+//     .then((res) => res.json())
+//     .then((data) => {
+//       console.log(data);
+//       jokeEl.innerHTML = data.joke;
+//     });
+// }
