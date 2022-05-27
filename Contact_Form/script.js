7b835e926a9803bdaf2ae734e5860d836be27ed9
Asadijaz786 <ijazasad20@gmail.com>
2022-05-27 22:40:21 +0500

    Contact Form: html&css m2
     project24

--- Contact_Form/script.js
+++ Contact_Form/script.js
@@ -1,1 +1,32 @@
 const progress = document.getElementById("progress");
+
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".close-modal");
+const btnSend = document.querySelector(".sendBtn");
+// console.log(btnsOpenModal);
+
+function closeModal() {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+}
+
+function openModal() {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+}
+
+btnSend.addEventListener("click", openModal);
+
+btnCloseModal.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
+
+// esc key press
+document.addEventListener("keydown", function (e) {
+  // console.log('a key pressed');
+  // console.log(e.key);
+
+  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+    closeModal();
+  }
+});
