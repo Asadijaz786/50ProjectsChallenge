1f8d700e5fb716b7c98152850183993435d0146a
Asadijaz786 <ijazasad20@gmail.com>
2022-05-28 10:49:08 +0500

    Contact Form: html&css m javascript m
     project24

--- Contact_Form/script.js
+++ Contact_Form/script.js
@@ -1,10 +1,7 @@
-const progress = document.getElementById("progress");
-
 const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
 const btnCloseModal = document.querySelector(".close-modal");
 const btnSend = document.querySelector(".sendBtn");
-// console.log(btnsOpenModal);
 
 function closeModal() {
   modal.classList.add("hidden");
@@ -21,11 +18,7 @@ btnSend.addEventListener("click", openModal);
 btnCloseModal.addEventListener("click", closeModal);
 overlay.addEventListener("click", closeModal);
 
-// esc key press
 document.addEventListener("keydown", function (e) {
-  // console.log('a key pressed');
-  // console.log(e.key);
-
   if (e.key === "Escape" && !modal.classList.contains("hidden")) {
     closeModal();
   }
